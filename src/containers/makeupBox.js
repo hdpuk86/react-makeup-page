317ca34541ee2791e6cb1086f9daed1eaf7d7cfc
hdpuk86 <hayley.d.prior@gmail.com>
2018-01-10 10:54:45 +0000

    adds api call

--- src/containers/makeupBox.js
+++ src/containers/makeupBox.js
@@ -5,6 +5,28 @@ import ProductBox from './productBox';
 
 class MakeupBox extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      products: [],
+      selectedBrand: "almay"
+    }
+  }
+
+  componentDidMount(){
+    const url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=" + this.state.selectedBrand;
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('load', () => {
+      if(request.status === 200){
+        const jsonString = request.responseText;
+        const data = JSON.parse(jsonString);
+        this.setState({products: data});
+      }
+    })
+    request.send();
+  }
+
   render(){
     return(
       <div className="makeup-box">
